fb3243cf5df2a4b11f9003c1fc59d3232b00b88f
tpsnt <4048510+tpsnt@users.noreply.github.com>
2024-02-29 10:42:08 +0800

    Update index.js
    Update index.js
    
    Correcting for PR comments
    
    Correct for PR

--- src/extensions/scratch3_wedo2/index.js
+++ src/extensions/scratch3_wedo2/index.js
@@ -1373,9 +1373,12 @@ class Scratch3WeDo2Blocks {
     startMotorPower (args) {
         // TODO: cast args.MOTOR_ID?
         this._forEachMotor(args.MOTOR_ID, motorIndex => {
-            const motor = this._peripheral.motor(motorIndex);
-            if (motor) {
-                motor.power = MathUtil.clamp(Cast.toNumber(args.POWER), 0, 100);
+            const power = MathUtil.clamp(Cast.toNumber(args.POWER), 0, 100);
+            if (power === 0) {
+                motor.turnOff();
+                motor.power = power;
+            } else {
+                motor.power = power;
                 motor.turnOn();
             }
         });
